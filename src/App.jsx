4f3323057a986510d1b7774dbeb4feae536cdcf5
mooni <mooni9375@gmail.com>
2026-04-05 11:41:34 +0900

    Routing init.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
 
 // 1. "/" : 모든 일기를 조회하는 Home Page
 // 2. "/new" : 새로운 일기를 작성하는 New Page
 // 3. "/diary" : 일기를 상세히 조회하는 Diary Page
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
 
 export default App;
