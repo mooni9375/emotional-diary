09ba827d93852db0424030bdc68c3b501477c101
mooni <mooni9375@gmail.com>
2026-04-05 11:27:12 +0900

    init commit.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+// 1. "/" : 모든 일기를 조회하는 Home Page
+// 2. "/new" : 새로운 일기를 작성하는 New Page
+// 3. "/diary" : 일기를 상세히 조회하는 Diary Page
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
