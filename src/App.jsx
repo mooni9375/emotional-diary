d34d7c5f535f37d301a823830b10365b5cea0469
mooni <mooni9375@gmail.com>
2026-04-05 13:19:47 +0900

    Import Images. (After Directory Seperated)

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 
-import emotion1 from "./assets/emotion1.png";
-import emotion2 from "./assets/emotion2.png";
-import emotion3 from "./assets/emotion3.png";
-import emotion4 from "./assets/emotion4.png";
-import emotion5 from "./assets/emotion5.png";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home Page
 // 2. "/new" : 새로운 일기를 작성하는 New Page
@@ -23,21 +19,12 @@ function App() {
 
   return (
     <>
-      {/* public 디렉토리에서 아래와 같이 불러오면 캐싱이 되지 않음 - 최적화에 불리 */}
-      {/*<div>*/}
-      {/*  <img src={"/emotion1.png"} alt="emotion1" />*/}
-      {/*  <img src={"/emotion2.png"} alt="emotion1" />*/}
-      {/*  <img src={"/emotion3.png"} alt="emotion1" />*/}
-      {/*  <img src={"/emotion4.png"} alt="emotion1" />*/}
-      {/*  <img src={"/emotion5.png"} alt="emotion1" />*/}
-      {/*</div>*/}
-      {/* 캐싱 가능하여 최적화에 유리 - 이미지가 많은 경우 캐싱시 스토리지 이슈 발생 가능하니 유의 */}
       <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
       </div>
       <div>
         <Link to={"/"}>Home</Link>
