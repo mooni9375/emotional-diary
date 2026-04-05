a0577bd5532fd8851ef38454ef85022276391b2f
mooni <mooni9375@gmail.com>
2026-04-05 11:53:07 +0900

    Link, useNavigate 적용 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
@@ -9,13 +9,27 @@ import Notfound from "./pages/Notfound";
 // 2. "/new" : 새로운 일기를 작성하는 New Page
 // 3. "/diary" : 일기를 상세히 조회하는 Diary Page
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
