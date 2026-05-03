b8f4effee42a7029888ba73454b79af6fcefc8c4
mooni <mooni9375@gmail.com>
2026-05-03 18:01:16 +0900

    삭제 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,41 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App.jsx";
+
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기 수정입니다.</div>;
+  const nav = useNavigate();
+
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("정말 삭제할까요?")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={
+          <Button
+            onClick={() => nav("/", { replace: true })}
+            text={"< 뒤로 가기"}
+          />
+        }
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
