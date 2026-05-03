e56ee26e3eec181fabb9d3e22150cd0e27e7f222
mooni <mooni9375@gmail.com>
2026-05-03 15:04:06 +0900

    목록 정렬 기능 적용

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -2,14 +2,37 @@ import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const DiaryList = ({ data }) => {
   const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    // return data.toSorted((a, b) => {
+    //   if (sortType === "oldest") {
+    //     return a.createdDate - b.createdDate;
+    //   } else {
+    //     return b.createdDate - a.createdDate;
+    //   }
+    // });
+    return data.toSorted((a, b) =>
+      sortType === "oldest"
+        ? Number(a.createdDate) - Number(b.createdDate)
+        : Number(b.createdDate) - Number(a.createdDate),
+    );
+  };
+
+  const sortedData = getSortedData();
 
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value="latest">최신순</option>
           <option value="oldest">오래된 순</option>
         </select>
@@ -20,7 +43,7 @@ const DiaryList = ({ data }) => {
         />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
